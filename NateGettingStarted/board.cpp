242ccbcd9701b91a09ef5407b3a3968621d341e5
Nate Schultz <nate.schultz@outlook.com>
2016-09-30 12:36:39 -0400

    more stuff

--- NateGettingStarted/board.cpp
+++ NateGettingStarted/board.cpp
@@ -13,17 +13,31 @@ Board::Board() {
     cout << "Board Initialized!" << endl;
 }
 
+vector<Piece> make_rank(Piece a, Piece b) {
+
+     vector<Piece> rank;
+
+     rank.push_back(a);
+     rank.push_back(b);
+ 
+     return rank;
+}
+
 void Board::init_pieces() {
 
      for (int i = 0; i < 8; i++ ) {
-          pieces.push_back();
+          pieces.push_back( { } );
      }
     	 
      //rank 1
-     pieces[0] = { new Piece('R','W'), new Piece('N','W'), new Piece('B','W'), new Piece('K','W'), new Piece('Q','W'), new Piece('B','W'), new Piece('N','W'), new Piece('R','W') };
+     Piece rook('R','W');
+     vector<Piece> rank1 = make_rank(rook, rook);
+     //rank1  = make_rank( new Piece('R','W'), new Piece('N','W'), new Piece('B','W'), new Piece('K','W'), new Piece('Q','W'), new Piece('B','W'), new Piece('N','W'), new Piece('R','W') );
      //rank 2
      
      //rank 7
 
      //rank 8  
+     pieces[0] = rank1;
 }
+
